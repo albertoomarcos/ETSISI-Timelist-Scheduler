03ac02891f0694526b62669ebf57f29f420be581
Alvaro <ac28042@gmail.com>
2018-04-14 12:54:49 +0200

    Commit

---
+++ app/src/main/java/com/upm/etsisi/acp/etsisitimelistscheduler/Main.kt
@@ -0,0 +1,15 @@
+package com.upm.etsisi.acp.etsisitimelistscheduler
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
